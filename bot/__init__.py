91348482513923b241b47e80b8b3b774618cba39
Developer: Mina Ernest Kamal  💻⚖️ <minajoneer5@gmail.com>
2025-07-14 17:06:42 +0300

    Add files via upload

---
+++ bot/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+الحزمة الرئيسية للبوت
+Main bot package
+"""
+
+__version__ = "1.0.0"
+__author__ = "Telegram Group Protection Bot"
+__description__ = "بوت حماية المجموعات في تليجرام"
